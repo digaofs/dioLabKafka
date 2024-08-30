b13b3d5b5078febdc2e7cdc8dd7b87853b3f645b
digaofs <rodrigofs.fs@gmail.com>
2024-08-30 16:04:43 -0300

    Commit inicial

---
+++ src/test/java/com/rfds/dio/bootcamp/kafkarest/KafkarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfds.dio.bootcamp.kafkarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
